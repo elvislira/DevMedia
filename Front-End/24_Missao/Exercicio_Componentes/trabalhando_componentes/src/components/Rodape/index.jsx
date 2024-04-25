ee36b2786b371ae39091ea5f31456b8638b556e7
Elvis Lira <professorelvislira@gmail.com>
2024-04-25 16:42:48 -0300

    Exercício - React - Missão 24 - Trabalhando com componentes.

---
+++ Front-End/24_Missao/Exercicio_Componentes/trabalhando_componentes/src/components/Rodape/index.jsx
@@ -0,0 +1,9 @@
+import estilos from '@/components/Rodape/Rodape.module.css';
+
+export default function Rodape() {
+    return (
+        <footer className={estilos.container_rodape}>
+            <h2>Desenvolvido com React</h2>
+        </footer>
+    )
+}
---
+++ Front-End/24_Missao/Exercicio_Componentes/trabalhando_componentes/src/components/Topo/index.jsx
@@ -0,0 +1,10 @@
+import estilos from "@/components/Topo/Topo.module.css";
+
+
+export default function Topo() {
+    return (
+        <header className={estilos.container_topo}>
+            <h1>Trabalhando com componentes</h1>
+        </header>
+    );
+}
