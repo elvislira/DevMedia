8f48cbb2f2961e67b4ec311f7aa6bb7f915f7d56
Elvis Lira <professorelvislira@gmail.com>
2024-04-27 17:21:17 -0300

    Alterações no projeto Ótica.

--- Front-End/24_Missao/projeto_otica/src/components/Rodape/index.jsx
+++ Front-End/24_Missao/projeto_otica/src/components/Rodape/index.jsx
@@ -4,7 +4,7 @@ import estilos from './Rodape.module.css';
 export default function Rodape() {
     return (
         <div className={estilos.rodape}>
-            <p>© 2022 Óticas Vida. - Todos os direitos reservados.</p>
+            <p>©2024 Óticas Vida. - Todos os direitos reservados.</p>
         </div>
     );
 }
